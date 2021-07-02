190da70b93bef9e224ce7634d455163ff593e701
Oleh Starodubets <65128735+starodubets-oleh@users.noreply.github.com>
2021-07-02 09:49:34 +0300

    remove context and add validation

--- src/components/NewTweetForm/TextAreaTweet/index.jsx
+++ src/components/NewTweetForm/TextAreaTweet/index.jsx
@@ -1,59 +1,78 @@
-import React, { useState, useCallback } from 'react';
+
+import React, { useCallback, useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import TextField from '@material-ui/core/TextField';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import FormControl from '@material-ui/core/FormControl';
-import { useTweets } from '../../../providers/tweets';
+import { Formik } from 'formik';
+import * as yup from 'yup';
+
+import { getLocalStorageUserName, getLocalStorageUserAvatar } from '../../../utils/localStorageHelpers';
+import {createTweet} from '../../../redux/actions/tweetsAction'
 
 import styles from './styles.module.scss';
 
 const TextAreaTweet = () => {
+  
+  const dispatch = useDispatch()
 
-  const { createTweet } = useTweets();
-
-  const [value, setValue] = useState('');
+  const validationSchema = yup.object().shape({
+    tweet: yup.string().typeError('Only as a string').min(3, 'short tweet').max(50, 'long tweet').required()
+  })
 
   const handleSend = useCallback(
-    async () => {
-      await createTweet(value);
-      setValue('');
+    async ({tweet}, {resetForm}) => {
+      dispatch(createTweet(tweet))
+      resetForm({})
     },
-    [value, createTweet],
-  );
-
-  const handleChange = useCallback(
-    (event) => {
-      event.preventDefault()
-      setValue(event.target.value);
-    },
-    [],
+    [dispatch],
   );
 
   return (
     <>
-      <Avatar>OS</Avatar>
+      <Avatar alt={getLocalStorageUserName()} src={getLocalStorageUserAvatar()} />
 
       <div className={styles.tweetInner}>
-        <FormControl fullWidth noValidate autoComplete="off">
-          <TextField
-            fullWidth
-            id="standard-textarea"
-            value={value}
-            onChange={handleChange}
-            placeholder="What's happening?"
-            multiline
-          />
-        </FormControl>
-        <div className={styles.sendTweet}>
-          <Button
-            onClick={handleSend}
-            variant="contained"
-            color="primary"
-            disabled={value.length === 0}
-          >
-            tweet
-          </Button>
-        </div>
+        <Formik
+          initialValues={
+            {
+              tweet: ''
+            }
+          }
+          onSubmit={(values, actions) => handleSend(values, actions)}
+          validateOnBlur
+          validationSchema={validationSchema}
+        >
+          {({ values, errors, touched, handleChange, handleBlur, isValid, handleSubmit, dirty }) => (
+            <>
+              <FormControl fullWidth noValidate autoComplete="off">
+                <TextField
+                  fullWidth
+                  type='text'
+                  name='tweet'
+                  value={values.tweet}
+                  onChange={handleChange}
+                  placeholder="What's happening?"
+                  onBlur={handleBlur}
+                  helperText={!dirty && touched.tweet && errors.tweet}
+                  error={!dirty && touched.tweet && errors.tweet}
+                  multiline
+                />
+              </FormControl>
+              <div className={styles.sendTweet}>
+                <Button
+                  onClick={handleSubmit}
+                  variant="contained"
+                  color="primary"
+                  disabled={!isValid}
+                >
+                  tweet
+                </Button>
+              </div>
+            </>
+          )}
+        </Formik>
       </div>
     </>
   );
